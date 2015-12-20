df42359d46325c8adb1b0cedc00b54471f50bd0a
sukelali <sukelali@gmail.com>
2015-12-20 20:14:09 +0600

    Basics ,events ,Controls,Styles,Drawing on the map,Layers

--- client/views/map.js
+++ client/views/map.js
@@ -1,11 +1,72 @@
-Template.map.helpers({
-	map: function(){
-      map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: {lat: -28, lng: 137}
-      });
-
-      // NOTE: This uses cross-domain XHR, and may not work on older browsers.
-      map.data.loadGeoJson('./test.json');
-    }
+Template.mapology.onRendered(function(){
+	GoogleMaps.load();
 });
+
+// Template.mapology.helpers({
+//   mapologyMapOptions: function() {
+//     // Make sure the maps API has loaded
+//     if (GoogleMaps.loaded()) {
+//       // Map initialization options
+//       return {
+//         center: new google.maps.LatLng(-37.8136, 144.9631),
+//         zoom: 8
+//       };
+//     }
+//   }
+// });
+
+
+Template.mapology.helpers({
+    mapologyMapOptions: function() {
+
+    	// Test purpose lat and lon  value setup.
+
+    	Session.set('lat',-37.8136);
+    	Session.set('lon',144.9631);
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+        var coords = [Session.get('lat'), Session.get('lon')]   
+      return {
+        center: new google.maps.LatLng(coords[0],coords[1]),
+        scrollwheel: false,
+        zoom: 14
+      };
+    }
+  }
+});
+
+Template.mapology.created = function () {
+    // getUserGeolocation();
+  GoogleMaps.ready('mapologyMap', function(map) {
+
+    Venues.find().forEach(function(doc){
+        var infowindow = new google.maps.InfoWindow({
+                content: [
+                    '<header>' + doc.location.name.capitalizeFirstLetter() + '</header>',
+                    '<main>' + doc.location.address.capitalizeFirstLetter() + '</main>'].join('')
+            });
+           var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(doc.location.lat, doc.location.lon),
+          map: map.instance
+        });
+        marker.set('title', doc.location.name);
+
+        marker.addListener('click',function(){
+        window.location.hash = '#t_' + doc._id;
+        infowindow.open(map.instance, marker);
+      });
+        });
+
+      var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(Session.get('lat'), Session.get('lon')),
+      icon: 'http://maps.gstatic.com/mapfiles/markers2/icon_green.png',
+      map: map.instance,
+      title: "Your Geolocation"
+    });     
+
+        $(window).on('resize', function(){
+            map.instance.setCenter(new google.maps.LatLng(Session.get('lat'), Session.get('lon')));
+        });
+
+  });
+};
--- routes/map.js
+++ routes/map.js
@@ -1,13 +1,4 @@
 Router.route('/map', {
-  name: 'map',
-  template: 'map'
-  // waitOn: function() { return [
-  //   Meteor.subscribe('Magicmoments')
-  //   ]
-  // },
-  // data: function() {
-  //   return {
-  //     magicmoments: Magicmoments.find({}, {sort: {createdAt: -1}})
-  //   }
-  // }
+  name: 'mapology',
+  template: 'mapology'
 });
